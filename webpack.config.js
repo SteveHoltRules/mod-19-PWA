ba373d34ec55c471c50dcf6b751c08e17df28942
Matt Luna <mat.c.luna@gmail.com>
2021-11-27 22:11:15 -0600

    Add Heroku

--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,16 @@
-// ES5
 const webpack = require("webpack");
 const { BundleAnalyzerPlugin } = require("webpack-bundle-analyzer");
-const WebpackPwaManifest = require('webpack-pwa-manifest')
+// const SWPrecacheWebpackPlugin = require("sw-precache-webpack-plugin");
+const WebpackPwaManifest = require("webpack-pwa-manifest");
 const path = require("path");
 
 const config = {
   entry: {
-    index: '.public/js/index.js'
+    index: "./public/js/index.js",
   },
-    output: {
-    filename: '[name].bundle.js',
-    path: `${__dirname}/dist`
+  output: {
+    filename: "[name].bundle.js",
+    path: `${__dirname}/dist`,
   },
   module: {
     rules: [
@@ -18,48 +18,48 @@ const config = {
         test: /\.(png|jpe?g|gif)$/i,
         use: [
           {
-            loader: 'file-loader',
+            loader: "file-loader",
             options: {
               esModule: false,
               name(file) {
-                return '[path][name].[ext]';
+                return "[path][name].[ext]";
               },
               publicPath(url) {
-                return url.replace('../', '/assets/');
-              }
-            }
+                return url.replace("../", "/assets/");
+              },
+            },
           },
           {
-            loader: 'image-webpack-loader'
-          }
-        ]
-      }
-    ]
-  },
-plugins: [
-  new webpack.ProvidePlugin({
-    $: "jquery",
-    jQuery: "jquery",
-  }),
-  new BundleAnalyzerPlugin({
-    analyzerMode: "static",
-  }),
-  new WebpackPwaManifest({
-    name: "Budget App",
-    short_name: "Budget",
-    description: "An App for Budgeting",
-    background_color: "#01579b",
-    theme_color: "#ffffff",
-    icons: [
-      {
-        src: path.resolve("public/icons/icon-512x512.png"),
-        sizes: [96, 128, 192, 256, 384, 512], // multiple sizes
-        destination: path.join("public", "icons"),
+            loader: "image-webpack-loader",
+          },
+        ],
       },
     ],
-  }),
-],
-mode: 'development'
+  },
+  plugins: [
+    new webpack.ProvidePlugin({
+      $: "jquery",
+      jQuery: "jquery",
+    }),
+    new BundleAnalyzerPlugin({
+      analyzerMode: "static",
+    }),
+    new WebpackPwaManifest({
+      name: "Budget App",
+      short_name: "Budget",
+      description: "An App for Budgeting",
+      background_color: "#01579b",
+      theme_color: "#ffffff",
+      icons: [
+        {
+          src: path.resolve("public/icons/icon-512x512.png"),
+          sizes: [96, 128, 192, 256, 384, 512], // multiple sizes
+          destination: path.join("public", "icons"),
+        },
+      ],
+    }),
+  ],
+  mode: "development",
 };
 
 module.exports = config;
